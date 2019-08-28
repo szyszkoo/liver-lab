1e76ef3833e244c3cf560c51d5bd7b3b6632b89d
Agata Szyszko <agata.szysz@gmail.com>
2019-08-28 22:09:48 +0200

    Added gaussian filtering

--- src/experiments.py
+++ src/experiments.py
@@ -5,6 +5,7 @@ from liverDataUtils.liverReader import LiverReader
 from liverDataUtils.plotter import Plotter
 from regionGrowing import RegionGrowing
 from liverDataUtils.fileNames import FileNames
+from skimage.filters import gaussian
 
 sampleNumber = "03"
 liverReader = LiverReader()
@@ -13,10 +14,11 @@ cubeInterpolator = CubeInterpolator()
 regionGrowing = RegionGrowing()
 
 liverData = liverReader.readInterpolatedLiverData(sampleNumber)
-regionGrowth = regionGrowing.grow(liverData, (107,136,142), 4)
+afterGauss = gaussian(liverData, sigma=0.5)
+regionGrowth = regionGrowing.grow(afterGauss, (107,136,142), 5)
 # interpolated = cubeInterpolator.interpolateCube(liverData, 2)
 
-Plotter(liverData, regionGrowth, 164)
+Plotter(afterGauss, regionGrowth, 164)
 # nrrd.write(fileNames.getInterpolatedCubeFileName(sampleNumber), interpolated)
 
 
