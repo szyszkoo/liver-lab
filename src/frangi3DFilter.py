93518843e6a98b7a4c5d7da559db7145aa69ea10
Damian <damian.g.borys@gmail.com>
2019-12-02 13:42:48 +0100

    Merge remote-tracking branch 'refs/remotes/origin/develop'
    Conflicts:
    	ROILIVER01.nrrd
    	displaySlices.py
    	myROI_LIVER2.nrrd
    	readData.py

--- src/frangi3DFilter.py
+++ src/frangi3DFilter.py
@@ -3,30 +3,68 @@ import matplotlib.pyplot as plt
 import nrrd
 
 from liverDataUtils.liverReader import LiverReader
+import sys
+sys.path.append("~/repo/liver-lab/frangi/")
+
+from frangi.frangi import frangi
 from liverDataUtils.plotter import Plotter
 
-import skimage as sk
-from skimage import exposure
+from skimage.filters import frangi as frangi2D
 
-from skimage.filters import (threshold_otsu, threshold_niblack, threshold_sauvola)
+# import skimage as sk
+# from skimage import exposure
 
-from skimage.morphology import disk
-from skimage.filters import rank
+# from skimage.filters import (threshold_otsu, threshold_niblack, threshold_sauvola)
+
+# from skimage.morphology import disk
+# from skimage.filters import rank
 # from skimage.filters import frangi, hessian
-import frangi as fr
-from fr import frangi
+# import frangi as fr
+# from fr import frangi
 
 
 
-sampleNumber = "01"
+sampleNumber = "03"
 liverReader = LiverReader()
+interp_liverData = liverReader.readInterpolatedLiverData(sampleNumber)
 liverData = liverReader.readLiverData(sampleNumber)
 roiData = liverReader.readLiverROIData(sampleNumber)
 
 
-liver01 = liverData / np.max(liverData)  #normalised Liver
+
+# Plotter(liverData, interp_liverData, 100)
+
+#
+# liver01 = liverData / np.max(liverData)  #normalised Liver
+#
+# livfrang = np.zeros((liverData.shape[0], liverData.shape[1], liverData.shape[2]), dtype=float)
+#
+livfrang = frangi(liverData)
+liverFrangi3D = np.multiply(livfrang, roiData)
+
+# plotter = Plotter(liverData, liverFrangi, 50)
+# plotter.draw()
 
 livfrang = np.zeros((liverData.shape[0], liverData.shape[1], liverData.shape[2]), dtype=float)
+for index in np.arange(0, liverData.shape[2], 1):
+    livfrang[:, :, index] = frangi2D(liverData[:, :, index])
+liverFrangi2D = np.multiply(livfrang, roiData)
 
-livfrang = frangi(liverData)
-liverFrangi = np.multiply(livfrang, roiData)
+plotter = Plotter(liverFrangi3D, liverFrangi2D, 50)
+plotter.draw()
+
+
+filename2D = 'liverFrangi2D_' + sampleNumber + '.nrrd'
+filename3D = 'liverFrangi3D_' + sampleNumber + '.nrrd'
+nrrd.write(filename2D, liverFrangi2D)
+nrrd.write(filename3D, liverFrangi3D)
+
+
+liverFrangi2_3 = np.multiply(liverFrangi2D , liverFrangi3D)
+
+plotter = Plotter(liverData, liverFrangi2_3, 50)
+plotter.draw()
+
+filename2_3 = 'liverFrangi2_3_' + sampleNumber + '.nrrd'
+nrrd.write(filename2_3, liverFrangi2_3)
+
--- src/frangiFilter.py
+++ src/frangiFilter.py
@@ -32,24 +32,25 @@ liverFrangi = np.multiply(livfrang, roiData)
 
 # Plotter(liverData, liverFrangi, 50)
 
-
+plotter = Plotter(liverData, liverFrangi, 50)
+plotter.draw()
 
 # Contrast stretching
-p2, p98 = np.percentile(liverData, (2, 98))
-img_rescale = exposure.rescale_intensity(liverData, in_range=(p2, p98))
-
-for index in np.arange(0, liverData.shape[2], 1):
-    livfrang[:, :, index] = frangi(img_rescale[:, :, index])
-liverFrangi_ImRescale = np.multiply(livfrang, roiData)
-
-# Plotter(img_rescale,liverFrangi_ImRescale, 50)
-
-
-# gamma corr
-gamma_corrected = exposure.adjust_gamma(liverData, 1.2)
-for index in np.arange(0, liverData.shape[2], 1):
-    livfrang[:, :, index] = frangi(gamma_corrected[:, :, index])
-liverFrangi_gamma = np.multiply(livfrang, roiData)
+# p2, p98 = np.percentile(liverData, (2, 98))
+# img_rescale = exposure.rescale_intensity(liverData, in_range=(p2, p98))
+#
+# for index in np.arange(0, liverData.shape[2], 1):
+#     livfrang[:, :, index] = frangi(img_rescale[:, :, index])
+# liverFrangi_ImRescale = np.multiply(livfrang, roiData)
+#
+# # Plotter(img_rescale,liverFrangi_ImRescale, 50)
+#
+#
+# # gamma corr
+# gamma_corrected = exposure.adjust_gamma(liverData, 1.2)
+# for index in np.arange(0, liverData.shape[2], 1):
+#     livfrang[:, :, index] = frangi(gamma_corrected[:, :, index])
+# liverFrangi_gamma = np.multiply(livfrang, roiData)
 
 # Plotter(img_rescale,liverFrangi_gamma, 50)
 
@@ -57,26 +58,26 @@ liverFrangi_gamma = np.multiply(livfrang, roiData)
 
 
 # Equalization (Global)
-img_eq = exposure.equalize_hist(liverData)
-
-for index in np.arange(0, liverData.shape[2], 1):
-    livfrang[:, :, index] = frangi(img_eq[:, :, index])
-liverFrangi_imgeq = np.multiply(livfrang, roiData)
+# img_eq = exposure.equalize_hist(liverData)
+#
+# for index in np.arange(0, liverData.shape[2], 1):
+#     livfrang[:, :, index] = frangi(img_eq[:, :, index])
+# liverFrangi_imgeq = np.multiply(livfrang, roiData)
 
 # Plotter(img_rescale,liverFrangi_imgeq, 50)
 
 
-filename = 'liverFrangi' + sampleNumber + '.nrrd'
-nrrd.write(filename, liverFrangi)
-
-filename = 'liverFrangi_ImRescale' + sampleNumber + '.nrrd'
-nrrd.write(filename, liverFrangi_ImRescale)
-
-filename = 'liverFrangi_gamma' + sampleNumber + '.nrrd'
-nrrd.write(filename, liverFrangi_gamma)
-
-filename = 'liverFrangi_imgeq' + sampleNumber + '.nrrd'
-nrrd.write(filename, liverFrangi_imgeq)
+# filename = 'liverFrangi' + sampleNumber + '.nrrd'
+# nrrd.write(filename, liverFrangi)
+#
+# filename = 'liverFrangi_ImRescale' + sampleNumber + '.nrrd'
+# nrrd.write(filename, liverFrangi_ImRescale)
+#
+# filename = 'liverFrangi_gamma' + sampleNumber + '.nrrd'
+# nrrd.write(filename, liverFrangi_gamma)
+#
+# filename = 'liverFrangi_imgeq' + sampleNumber + '.nrrd'
+# nrrd.write(filename, liverFrangi_imgeq)
 
 
 
