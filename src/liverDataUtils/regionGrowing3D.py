ed9b33b70ed81a5d1d5e1522df21735ed3741f36
Agata Szyszko <agata.szysz@gmail.com>
2020-01-05 15:32:37 +0100

    Test region growing masks added - region growing from selected seed, on result of Frangi 2D filter

--- src/liverDataUtils/regionGrowing3D.py
+++ src/liverDataUtils/regionGrowing3D.py
@@ -4,6 +4,7 @@ from liverDataUtils.regionGrowing import RegionGrowing
 from liverDataUtils.plotter import Plotter
 import nrrd
 import time
+import numpy as np 
 
 start_time = time.time()
 sampleNumber = "03"
@@ -11,14 +12,21 @@ liverReader = LiverReader()
 fileNames = FileNames()
 regionGrowing = RegionGrowing()
 
-liverDataFrangi = liverReader.readNrrdData("testFrangi3D.nrrd")
-liverData = liverReader.readInterpolatedLiverData(sampleNumber)
-regionMask = regionGrowing.grow(liverData,(121,148,147), 6)
+liverDataFrangi = liverReader.readNrrdData("testFrangi2D.nrrd")
+roi = liverReader.readInterpolatedLiverData("_03_roi")
 
+liverDataFrangi = np.multiply(liverDataFrangi, roi)
+regionMask = regionGrowing.grow(liverDataFrangi,(120,149,147), 5)
+
+liverData = liverReader.readInterpolatedLiverData(sampleNumber)
+119-134,75,159
 print("Execution time [s]: ", time.time() - start_time)
 
+# regionMask[:,149,147] = 1
+# regionMask[120,:,147] = 1
 
-plotter = Plotter(regionMask, liverData, 150)
+nrrd.write("testRegionGrowingMask_nbgh5.nrrd", regionMask.astype(int)*255)
+
+plotter = Plotter(regionMask, liverDataFrangi, 150)
 plotter.draw()
 
-# nrrd.write("testRegionGrowingMask.nrrd", regionMask)
